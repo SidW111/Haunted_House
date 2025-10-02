1a4855068a8641fbb22b1b509962c2832347b502
SidW111 <siddhantwainngade@gmail.com>
2025-10-02 21:00:10 +0530

    house

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import GUI from "lil-gui";
 
 //gui
-const gui  = new GUI()
+const gui = new GUI();
 
 //scene
 const scene = new THREE.Scene();
@@ -18,20 +18,65 @@ scene.add(directionalLight);
 
 //object
 
-//sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1,32,32),
-    new THREE.MeshStandardMaterial({roughness:0.7})
-);
-
 //floor
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20,20),
-    new THREE.MeshStandardMaterial()
-)
-scene.add(floor)
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial()
+);
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
 
-scene.add(sphere);
+//house
+const house = new THREE.Group();
+scene.add(house);
+
+//walls
+const walls = new THREE.Mesh(
+  new THREE.BoxGeometry(4, 2.5, 4),
+  new THREE.MeshStandardMaterial()
+);
+walls.position.y += 1.25;
+house.add(walls);
+
+//roof
+const roof = new THREE.Mesh(
+  new THREE.ConeGeometry(3.5, 1.5, 4),
+  new THREE.MeshStandardMaterial()
+);
+roof.position.y = 2.5 + 0.75;
+roof.rotation.y = Math.PI * 0.25;
+house.add(roof);
+
+//door
+const door = new THREE.Mesh(
+  new THREE.PlaneGeometry(2.2, 2.2),
+  new THREE.MeshStandardMaterial({ color: "red" })
+);
+door.position.z = 2 + 0.01;
+door.position.y = 1;
+house.add(door);
+
+//bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshStandardMaterial();
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(0.8, 0.2, 2.2);
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.25, 0.25, 0.25);
+bush2.position.set(1.4, 0.1, 2.1);
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(-0.8, 0.1, 2.2);
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15);
+bush4.position.set(-1, 0.05, 2.6);
+
+house.add(bush1, bush2, bush3, bush4);
 
 //camera
 const canvas = document.querySelector(".webgl");
