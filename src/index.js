1b4bd4224523f8e500447c06b474e5cbbe276302
SidW111 <siddhantwainngade@gmail.com>
2025-10-05 18:08:29 +0530

    fog

--- src/index.js
+++ src/index.js
@@ -395,6 +395,9 @@ sky.material.uniforms["mieDirectionalG"].value = 0.95;
 sky.material.uniforms["turbidity"].value = 10;
 sky.material.uniforms["sunPosition"].value.set(0.3,-0.038,-0.95)
 
+//fog
+
+scene.fog = new THREE.FogExp2('#09343f',0.1)
 
 const timer = new THREE.Timer();
 const tick = () => {
